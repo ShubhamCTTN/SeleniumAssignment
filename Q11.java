1b43f9deefcc379f204a33b41d101fe59dcaf10b
ShubhamCTTN <114070622+ShubhamCTTN@users.noreply.github.com>
2022-10-17 23:42:36 +0530

    Add files via upload

---
+++ Q11.java
@@ -0,0 +1,18 @@
+/*Q11.  Write an automation script for filling all fields of the registration page. (http://demo.automationtesting.in/Register.html)
+
+   a- Validate that Refresh button is disabled.
+   b- Validate that your respective gender is selected or not.
+   c- Validate that header of the registration page is Automation Demo Site. 
+   d- Validate that title of the page is Register.
+*/
+
+package selAssignment1;
+
+public class Q11 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ Q12.java
@@ -0,0 +1,14 @@
+/*Q12. Write an automation script for filling FirstName, LastName & Email address field with 
+the help of name locator. (http://demo.automationtesting.in/Register.html)
+*/
+
+package selAssignment1;
+
+public class Q12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
